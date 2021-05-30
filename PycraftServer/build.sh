4b7dca4685f8b2b2388f49787b0c45d590c702e7
Mike C. Fletcher <mcfletch@vrplumber.com>
2021-05-30 18:31:42 -0400

    Update build script for current release

--- PycraftServer/build.sh
+++ PycraftServer/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash 
 
 mvn -e package
-cp target/PycraftServer-1.0-SNAPSHOT.jar ../../scratch-world/plugins/
-cp target/PycraftServer-1.0-SNAPSHOT.jar ../../moosh-world/plugins/
-cp target/PycraftServer-1.0-SNAPSHOT.jar ../../slime-world/plugins/
+cp target/PycraftServer-1.0.0.jar ../../scratch-world/plugins/
+cp target/PycraftServer-1.0.0.jar ../../moosh-world/plugins/
+cp target/PycraftServer-1.0.0.jar ../../slime-world/plugins/
